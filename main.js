6a3020df9c02d3ca1dfa00ac1e8acafdf6b4aa6e
JoshuaJHaines <joshjhaines3@gmail.com>
2021-03-05 10:08:23 -0500

    Created 'repeatIt' funciton. Does NOT pass tests. moving on for now

--- main.js
+++ main.js
@@ -2,134 +2,140 @@
  * YOUR CODE HERE *
  ******************/
 
-function xify(str){
-  newStr = ''
-  for (letter of str){
-    newStr += 'x'
-  }
-  return newStr
+function xify(str) {
+	newStr = "";
+	for (letter of str) {
+		newStr += "x";
+	}
+	return newStr;
 }
 
-function yellingChars(str){
-  newStr = ''
-  for (letter of str){
-    newStr += letter + '!'
-  }
-  return newStr
+function yellingChars(str) {
+	newStr = "";
+	for (letter of str) {
+		newStr += letter + "!";
+	}
+	return newStr;
 }
 
-function indexedChars(str){
-  newStr = ''
-  for (i = 0; i < str.length; i++){
-    newStr += i + str[i]
-  }
-  return newStr
+function indexedChars(str) {
+	newStr = "";
+	for (i = 0; i < str.length; i++) {
+		newStr += i + str[i];
+	}
+	return newStr;
 }
 
-function numberedChars(str){
-  newStr = ''
-  for (i = 0; i < str.length; i++){
-    newStr += `(${i + 1})` + str[i]
-  }
-  return newStr
+function numberedChars(str) {
+	newStr = "";
+	for (i = 0; i < str.length; i++) {
+		newStr += `(${i + 1})` + str[i];
+	}
+	return newStr;
 }
 
-function exclaim(str){
-  newStr = ''
-  for (char of str){
-    if (char === "?" ||
-    char === "."){
-      newStr += "!"
-    } else {
-      newStr += char
-    }
-  }
-  return newStr
+function exclaim(str) {
+	newStr = "";
+	for (char of str) {
+		if (char === "?" || char === ".") {
+			newStr += "!";
+		} else {
+			newStr += char;
+		}
+	}
+	return newStr;
 }
 
+//Not Working yet and getting annoyed...moving on for now
+function repeatIt(str, n) {
+	newStr = "";
+	for (i = 0; i >= n; i++) {
+		newStr = newStr + str;
+	}
+	return newStr;
+}
 
+function truncate(str) {}
 
-
- /********************************************
+/********************************************
  * CODE DOWN HERE IS FOR INTERNAL USE ONLY. *
  *           PLEASE DON'T TOUCH!            *
  ********************************************/
 
-if (typeof xify === 'undefined') {
-  xify = undefined;
+if (typeof xify === "undefined") {
+	xify = undefined;
 }
 
-if (typeof smilify === 'undefined') {
-  smilify = undefined;
+if (typeof smilify === "undefined") {
+	smilify = undefined;
 }
 
-if (typeof yellingChars === 'undefined') {
-  yellingChars = undefined;
+if (typeof yellingChars === "undefined") {
+	yellingChars = undefined;
 }
 
-if (typeof numberedChars === 'undefined') {
-  numberedChars = undefined;
+if (typeof numberedChars === "undefined") {
+	numberedChars = undefined;
 }
 
-if (typeof indexedChars === 'undefined') {
-  indexedChars = undefined;
+if (typeof indexedChars === "undefined") {
+	indexedChars = undefined;
 }
 
-if (typeof exclaim === 'undefined') {
-  exclaim = undefined;
+if (typeof exclaim === "undefined") {
+	exclaim = undefined;
 }
 
-if (typeof repeatIt === 'undefined') {
-  repeatIt = undefined;
+if (typeof repeatIt === "undefined") {
+	repeatIt = undefined;
 }
 
-if (typeof truncate === 'undefined') {
-  truncate = undefined;
+if (typeof truncate === "undefined") {
+	truncate = undefined;
 }
 
-if (typeof ciEmailify === 'undefined') {
-  ciEmailify = undefined;
+if (typeof ciEmailify === "undefined") {
+	ciEmailify = undefined;
 }
 
-if (typeof reverse === 'undefined') {
-  reverse = undefined;
+if (typeof reverse === "undefined") {
+	reverse = undefined;
 }
 
-if (typeof onlyVowels === 'undefined') {
-  onlyVowels = undefined;
+if (typeof onlyVowels === "undefined") {
+	onlyVowels = undefined;
 }
 
-if (typeof crazyCase === 'undefined') {
-  crazyCase = undefined;
+if (typeof crazyCase === "undefined") {
+	crazyCase = undefined;
 }
 
-if (typeof titleCase === 'undefined') {
-  titleCase = undefined;
+if (typeof titleCase === "undefined") {
+	titleCase = undefined;
 }
 
-if (typeof camelCase === 'undefined') {
-  camelCase = undefined;
+if (typeof camelCase === "undefined") {
+	camelCase = undefined;
 }
 
-if (typeof crazyCase2ReturnOfCrazyCase === 'undefined') {
-  crazyCase2ReturnOfCrazyCase = undefined;
+if (typeof crazyCase2ReturnOfCrazyCase === "undefined") {
+	crazyCase2ReturnOfCrazyCase = undefined;
 }
-
 
 module.exports = {
-  xify,
-  smilify,
-  indexedChars,
-  yellingChars, // <-- add this line here (no need for the comment, obvs)
-  numberedChars,
-  exclaim,
-  repeatIt,
-  truncate,
-  ciEmailify,
-  reverse,
-  onlyVowels,
-  crazyCase,
-  titleCase,
-  camelCase,
-  crazyCase2ReturnOfCrazyCase,
-}
+	xify,
+	smilify,
+	indexedChars,
+	yellingChars, // <-- add this line here (no need for the comment, obvs)
+	numberedChars,
+	exclaim,
+	repeatIt,
+	truncate,
+	ciEmailify,
+	reverse,
+	onlyVowels,
+	crazyCase,
+	titleCase,
+	camelCase,
+	crazyCase2ReturnOfCrazyCase,
+};
